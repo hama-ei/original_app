45128911f3be89b555ae4736ebcec0b00142ea63
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-07-21 14:35:53 +0900

    create-file
    ファイルの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  protect_from_forgery with: :exception
+  before_action :basic
+
+  private
+  def basic
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "kyousitu" && password == "2019"
+    end
+  end
 end
