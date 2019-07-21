a7dd135959dba5f0bb8923db3942227b3bff541f
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-07-21 13:51:55 +0900

    create-album_function
    アルバム機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get 'users/sign_out' => 'devise/sessions#destroy'
   end
+
   root 'images#index'
-  resources :images
+
+  resources :images 
+
+  resources :users
+
+  get "users/album_show/:id", to: 'users#album_show', as: 'user_album_show'
+
 end
