6c9a7d303295f5bbd25e8415e4b44635eb8a77f2
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-07-25 10:49:51 +0900

    edit-column
    imagesテーブルカラムの修正

--- db/migrate/20190717041537_create_images.rb
+++ db/migrate/20190717041537_create_images.rb
@@ -1,12 +1,11 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.text      :image,         null:false
+      t.string      :image,         null:false
       t.integer   :user_id,       null:false
-      t.integer   :grade_id,      null:false
-      t.integer   :class_day_id,  nlll:false
-      t.integer   :class_time_id, null:false
-      t.date      :date,          null:false
+      t.integer      :date_year,          null:false
+      t.integer      :date_month,          null:false
+      t.integer      :date_day,          null:false
       t.timestamps
     end
   end
