9753c87c571d18a8ca8ebb559cfd78985c3c7232
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-10-17 13:39:12 +0900

    aaa
    aaa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
  
 CarrierWave.configure do |config|
-  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'hudenozikan'
@@ -14,10 +13,4 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1',
       path_style: true
     }
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
-  end
 end
- 
-CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
