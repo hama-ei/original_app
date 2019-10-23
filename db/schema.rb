a59a21a16bcefece50384a91f0e8c2c3c5b052b7
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-10-24 00:43:39 +0900

    aaa
    aaa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_043350) do
+ActiveRecord::Schema.define(version: 2019_07_17_043600) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_07_17_043350) do
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "image_id", null: false
     t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "name_kana", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
